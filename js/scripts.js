1855d09d8b48e294f671b46e3499985f39e74e68
Amisi Jospin Hassan <40860198+AmisiJospin@users.noreply.github.com>
2021-12-06 13:14:05 +0200

    This is the update of the website so far..

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
 */
 //
-// Scripts
+// Script
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
